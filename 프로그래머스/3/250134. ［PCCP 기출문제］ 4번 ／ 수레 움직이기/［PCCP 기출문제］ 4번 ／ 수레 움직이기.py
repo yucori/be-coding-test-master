8d85a96415656d7360800f81dc8e283f64658280
JiwonJang <julia6819@catholic.ac.kr>
2026-02-22 12:59:23 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 8.40 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
@@ -1,76 +1,44 @@
 from collections import deque
 
 def solution(maze):
-
-    def bfs(start, end):
-        q = deque()
-        q.append([start, [start]])
-        paths = []
-        
-        while q:
-            now, path = q.popleft()
-            if end == now:
-                paths.append(path)
-                continue
-            if len(path) > 16:
-                continue
-            for move in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                current = [now[0] + move[0], now[1] + move[1]]
-                if 0 <= current[0] < n and 0 <= current[1] < m:
-                    if current not in walls and current not in path:
-                        q.append([current, path+[current]])
-        return paths
-
-    n = len(maze)
-    m = len(maze[0])
-    walls = []
+    n, m = len(maze), len(maze[0])
 
     for i in range(n):
         for j in range(m):
-            check = maze[i][j]
-            if check == 1:
-                red_start = [i, j]
-            elif check == 2:
-                blue_start = [i, j]
-            elif check == 3:
-                red_end = [i, j]
-            elif check == 4:
-                blue_end = [i, j]
-            elif check == 5:
-                walls.append([i,j])
+            if maze[i][j] == 1: red_start = (i, j)
+            elif maze[i][j]  == 2: blue_start = (i, j)
+            elif maze[i][j] == 3: red_end = (i, j)
+            elif maze[i][j] == 4: blue_end = (i, j)
 
-    answer = 17
+    q = deque([(red_start, blue_start, {red_start}, {blue_start}, 0)])
 
-    # 빨강과 파랑의 모든 경로를 BFS로 탐색해서 저장해둔다
-    red_paths = bfs(red_start, red_end)
-    blue_paths = bfs(blue_start, blue_end)
+    while q:
+        r_now, b_now, r_visited, b_visited, cnt = q.popleft()
 
-    if not red_paths or not blue_paths:
-        return 0
+        if r_now == red_end and b_now == blue_end:
+            return cnt
 
-    # 조건들을 비교
-    for red_path in red_paths:
-        for blue_path in blue_paths:
-            if len(red_path) > len(blue_path):
-                long_path = red_path
-                short_path = blue_path
-            else:
-                long_path = blue_path
-                short_path = red_path
+        r_candidates = [r_now] if r_now == red_end else []
+        if not r_candidates:
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr, nc = r_now[0] + dx, r_now[1] + dy
+                if 0 <= nr < n and 0 <= nc < m and maze[nr][nc] != 5 and (nr, nc) not in r_visited:
+                    r_candidates.append((nr, nc))
 
-            for lp in range(len(long_path) - 1):
-                if lp < len(short_path)-1:
-                    sp = lp
-                else: 
-                    sp = -1
-
-                if long_path[lp] == short_path[sp]:
-                    break
-
-                if sp != -1:
-                    if long_path[lp + 1] == short_path[sp] and long_path[lp] == short_path[sp+1]:
-                        break
-            # 정상적으로 for을 모두 통과한 경우에만 answer 갱신
-            else:
-                answer = min(answer, len(long_path)-1)
-    return answer if answer != 17 else 0
+        b_candidates = [b_now] if b_now == blue_end else []
+        if not b_candidates:
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr, nc = b_now[0] + dx, b_now[1] + dy
+                if 0 <= nr < n and 0 <= nc < m and maze[nr][nc] != 5 and (nr, nc) not in b_visited:
+                    b_candidates.append((nr, nc))
+                    
+        for r_next in r_candidates:
+            for b_next in b_candidates:
+                if r_next == b_next: continue
+                if r_next == b_now and b_next == r_now: continue
+                
+                new_r_v = r_visited | {r_next}
+                new_b_v = b_visited | {b_next}
+                
+                q.append((r_next, b_next, new_r_v, new_b_v, cnt+1))
+    return 0
